d3be15add7b46f62b3aa3b97755f7d6dd74e813e
tgalen <tgalen84@gmail.com>
2022-07-11 17:20:42 -0400

    begin light/dark style implimentation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("Light");
+
   return (
-    <div>
-      <Header />
+    <div className="app" data-theme={theme}>
+      <Header theme={theme} setTheme={setTheme} />
+      <div className="accordian">
+        <button type="button" class="category-btn">
+          Category
+        </button>
+        <table className="category-table">
+          <tr>
+            <th>Price</th>
+            <th>ID</th>
+            <th>Chain</th>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>444444</td>
+            <td>DFK</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ theme, setTheme }) => {
+  const changeTheme = () => {
+    const newTheme = theme === "Light" ? "Dark" : "Light";
+    setTheme(newTheme);
+  };
+
   return (
     <h1>
-      DFK Floor Heroes<button>L/D</button>
+      DFK Floor Heroes<button onClick={changeTheme}>L/D</button>
     </h1>
   );
 };
