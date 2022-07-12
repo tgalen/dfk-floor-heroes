1b85bd102624646731ecc337305b5ea2750d0451
tgalen <tgalen84@gmail.com>
2022-07-12 19:41:26 -0400

    merge floor hero categories from both chains

--- src/components/Accordian/Accordian.js
+++ src/components/Accordian/Accordian.js
@@ -7,15 +7,44 @@ import "./Accordian.css";
 const Accordian = () => {
   const [floorHeroes, setFloorHeroes] = useState(null);
   const { loading, error, data } = useQuery(GET_FLOOR_HERO_PRICES);
+  const mergedFloorHeroCategories = {};
   useEffect(() => {
     if (data) {
       setFloorHeroes(data);
     }
   }, [data]);
+
+  floorHeroes &&
+    Object.keys(floorHeroes).forEach((category) => {
+      let mergedCategory = category.slice(0, -3);
+      if (mergedFloorHeroCategories[mergedCategory]) {
+        mergedFloorHeroCategories[mergedCategory] = mergedFloorHeroCategories[
+          mergedCategory
+        ].concat(floorHeroes[category]);
+      } else {
+        mergedFloorHeroCategories[mergedCategory] = floorHeroes[category];
+      }
+    });
+
+  floorHeroes && console.log(mergedFloorHeroCategories);
+
+  // floorHeroes &&
+  // Object.keys(floorHeroes).forEach((category) => {
+  //   let allChainCategory = category.slice(0, -3);
+  //   if (floorHeroesFromEachChain[allChainCategory]) {
+  //     floorHeroesFromEachChain[allChainCategory] = floorHeroesFromEachChain[
+  //       allChainCategory
+  //     ].concat(floorHeroes[category]);
+  //   } else {
+  //     floorHeroesFromEachChain[allChainCategory] = floorHeroes[category];
+  //   }
+  // });
+
   if (loading) return <div>Loading</div>;
   if (error) return <div>Error</div>;
 
   floorHeroes && console.log(floorHeroes);
+
   return (
     <div className="accordian">
       {/* map each hero cat */}
