48eb0608b2b6464b46859299fbadc9cca0abe86d
Big Al <alex.hamlett0@gmail.com>
2022-07-20 16:09:27 -0600

    added professions

--- src/components/HeroCategory/HeroCategory.js
+++ src/components/HeroCategory/HeroCategory.js
@@ -54,6 +54,7 @@ const HeroCategory = ({ category, floorHeroes, crystalJewelPair }) => {
                 <th>Rarity</th>
                 <th>Gen</th>
                 <th>Summons</th>
+                <th>Profession</th>
               </tr>
             </thead>
             <tbody>
