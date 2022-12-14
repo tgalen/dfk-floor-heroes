387eb7e426acc5c2840a76ebf1ad3098f151701f
tgalen <tgalen84@gmail.com>
2022-12-14 18:11:14 -0500

    add jewel to jade conversion display

--- src/App.js
+++ src/App.js
@@ -4,28 +4,44 @@ import Header from "./components/Header/Header";
 import Accordion from "./components/Accordion/Accordion";
 import TokenConversion from "./components/TokenConversion/TokenConversion";
 import "./App.css";
-import { CRYSTAL_JEWEL_PAIR_ENDPOINT_URL, JEWEL_JADE_PAIR_ENDPOINT_URL } from "./constants/constants";
+import {
+  CRYSTAL_JEWEL_PAIR_ENDPOINT_URL,
+  JEWEL_JADE_PAIR_ENDPOINT_URL,
+} from "./constants/constants";
 
 function App() {
   const [theme, setTheme] = useState(
     JSON.parse(localStorage.getItem("displayMode")) || "light"
   );
   const [crystalJewelPair, setCrystalJewelPair] = useState(null);
+  const [jewelJadePair, setJewelJadePair] = useState(null);
+
+  const getPairs = async () => {
+    const responseCJ = await fetch(CRYSTAL_JEWEL_PAIR_ENDPOINT_URL);
+    const crystalJewelPairData = await responseCJ.json();
+    setCrystalJewelPair(crystalJewelPairData.pair);
+
+    const responseJJ = await fetch(JEWEL_JADE_PAIR_ENDPOINT_URL);
+    const jewelJadePairData = await responseJJ.json();
+    console.log(jewelJadePairData);
+    setJewelJadePair(jewelJadePairData.pair);
+  };
 
   useEffect(() => {
-    const getCrystalJewelPair = async () => {
-      const response = await fetch(CRYSTAL_JEWEL_PAIR_ENDPOINT_URL);
-      const crystalJewelPairData = await response.json();
-      setCrystalJewelPair(crystalJewelPairData.pair);
-    };
-    getCrystalJewelPair();
+    getPairs();
   }, []);
 
   return (
     <div className="app" data-theme={theme}>
       <Header theme={theme} setTheme={setTheme} />
-      <Accordion crystalJewelPair={crystalJewelPair} />
-      <TokenConversion crystalJewelPair={crystalJewelPair} />
+      <Accordion
+        crystalJewelPair={crystalJewelPair}
+        jewelJadePair={jewelJadePair}
+      />
+      <TokenConversion
+        crystalJewelPair={crystalJewelPair}
+        jewelJadePair={jewelJadePair}
+      />
     </div>
   );
 }
--- src/components/TokenConversion/TokenConversion.js
+++ src/components/TokenConversion/TokenConversion.js
@@ -4,15 +4,21 @@ import JADE_ICON from "../../assets/jade.png";
 
 import "./TokenConversion.css";
 
-const TokenConversion = ({ crystalJewelPair }) => {
-  const converJewelToCrystal =
+const TokenConversion = ({ crystalJewelPair, jewelJadePair }) => {
+  const convertJewelToCrystal =
     crystalJewelPair && (1 / crystalJewelPair.priceNative).toFixed(3);
+
+  const convertJewelToJAde =
+    jewelJadePair && (1 / jewelJadePair.priceNative).toFixed(3);
 
   return (
     <h5>
       1 <img src={JEWEL_ICON} alt="JEWEL" className="token-icon" /> ={" "}
-      {crystalJewelPair && converJewelToCrystal}
+      {crystalJewelPair && convertJewelToCrystal}
       <img src={CRYSTAL_ICON} alt="CRYSTAL" className="token-icon" />
+      1 <img src={JEWEL_ICON} alt="JEWEL" className="token-icon" /> ={" "}
+      {jewelJadePair && convertJewelToJAde}{" "}
+      <img src={JADE_ICON} alt="JADE" className="token-icon" />
     </h5>
   );
 };
