56229893f272ed4c89ca27b0dc3f3a3251bb6932
tgalen <tgalen84@gmail.com>
2022-12-14 17:42:22 -0500

    rename conversion function

--- src/components/TokenConversion/TokenConversion.js
+++ src/components/TokenConversion/TokenConversion.js
@@ -5,13 +5,13 @@ import JADE_ICON from "../../assets/jade.png";
 import "./TokenConversion.css";
 
 const TokenConversion = ({ crystalJewelPair }) => {
-  const convertCrystalToJewel =
+  const converJewelToCrystal =
     crystalJewelPair && (1 / crystalJewelPair.priceNative).toFixed(3);
 
   return (
     <h5>
       1 <img src={JEWEL_ICON} alt="JEWEL" className="token-icon" /> ={" "}
-      {crystalJewelPair && convertCrystalToJewel}
+      {crystalJewelPair && converJewelToCrystal}
       <img src={CRYSTAL_ICON} alt="CRYSTAL" className="token-icon" />
     </h5>
   );
